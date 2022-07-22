a978a5eb8c07f682a4d98d62952a68ae771cacbf
CaptainBBB <101925316+CaptainBBB@users.noreply.github.com>
2022-07-22 19:54:26 +0300

    Add files via upload

---
+++ GitHub.py
@@ -0,0 +1,52 @@
+from string import ascii_lowercase, digits
+
+
+class TextInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    @classmethod
+    def check_name(cls, name):
+        return 3 <= len(name) <= 50 and all(a_letter in cls.CHARS_CORRECT for a_letter in name)
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+
+class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    @classmethod
+    def check_name(cls, name):
+        return 3 <= len(name) <= 50 and all(a_letter in cls.CHARS_CORRECT for a_letter in name)
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+    def render_template(self):
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+
+
+login = FormLogin(TextInput("Имя пользователя"), PasswordInput("Пароль пользователя"))
+html = login.render_template()
